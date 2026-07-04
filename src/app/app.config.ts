cf2567ba33305fded8cab689ea88b090a8f3c0b3
Kevin <kevin.bevenhall@gmail.com>
2026-07-04 22:27:45 +0200

    Transloco install

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,14 @@
 import { provideFileRouter, requestContextInterceptor, withExtraRoutes } from '@analogjs/router';
 import { provideHttpClient, withInterceptors, } from '@angular/common/http';
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, isDevMode, } from '@angular/core';
 import { routes } from './app.routes';
 import { provideRouter, withComponentInputBinding, withViewTransitions } from '@angular/router';
 import {
   provideTanStackQuery,
   QueryClient,
-} from '@tanstack/angular-query-experimental'
+} from '@tanstack/angular-query-experimental';
+import { TranslocoHttpLoader } from './transloco-loader';
+import { provideTransloco } from '@jsverse/transloco'
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -16,7 +18,16 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes, withViewTransitions(), withComponentInputBinding()),
     provideHttpClient(
       withInterceptors([requestContextInterceptor])
-    ),
+    ), provideHttpClient(), provideTransloco({
+        config: { 
+          availableLangs: ['en', 'sv'],
+          defaultLang: 'en',
+          // Remove this option if your application doesn't support changing language in runtime.
+          reRenderOnLangChange: true,
+          prodMode: !isDevMode(),
+        },
+        loader: TranslocoHttpLoader
+      }),
     //provideClientHydration(withEventReplay()),
   ],
 };
