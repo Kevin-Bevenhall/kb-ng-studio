fc2cc640a12f6443ba8bfb0cbd70d97861a79f71
Kevin <kevin.bevenhall@gmail.com>
2026-07-11 16:02:11 +0200

    List page layout component

---
+++ src/app/core/layout/list-page-layout/list-page-layout.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-page-layout',
+  imports: [],
+  templateUrl: './list-page-layout.component.html',
+  styleUrl: './list-page-layout.component.scss',
+})
+export class ListPageLayoutComponent {
+
+}
--- src/app/features/todos/pages/todo-list-page/todo-list-page.component.ts
+++ src/app/features/todos/pages/todo-list-page/todo-list-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { TitleComponent } from 'src/app/core/components/title/title.component';
 import { TodoListComponent } from '../../components/todo-list/todo-list.component';
+import { ListPageLayoutComponent } from 'src/app/core/layout/list-page-layout/list-page-layout.component';
 
 @Component({
   selector: 'app-todo-list-page',
-  imports: [TodoListComponent, TitleComponent],
+  imports: [TodoListComponent, TitleComponent, ListPageLayoutComponent],
   templateUrl: './todo-list-page.component.html',
   styleUrl: './todo-list-page.component.scss',
 })
